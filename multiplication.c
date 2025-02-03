12fda8a3de53bf17fec543f0786ab504cf5d365f
Tanvila Anjum Rabti <rabticse64@gmail.com>
2025-02-03 11:15:14 +0600

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int x = multiple();
+    printf ("%d",x);
+    return 0 ;
+}
+int multiple ()
+{
+    int a = 20;
+    int b = 40;
+    int res = a*b;
+    return res ;
+}
