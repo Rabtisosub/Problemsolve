d0587ae1eb8cbda094ddbf8ce65bfaf0598b446e
Tanvila Anjum Rabti <rabticse64@gmail.com>
2025-02-03 11:15:56 +0600

    Create division.c

---
+++ division.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int x = division();
+    printf ("%d",x);
+    return 0;
+}
+int division ()
+{
+    int a = 40;
+    int b = 20;
+    int res = a/b;
+    return res ;
+}
