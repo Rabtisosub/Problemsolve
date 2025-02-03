56ab610d95921781593b44f829f34432a3d465ba
Tanvila Anjum Rabti <rabticse64@gmail.com>
2025-02-03 11:14:25 +0600

    Create subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int x = minus();
+    printf("%d",x);
+    return 0;
+}
+int minus ()
+{
+    int a = 40;
+    int b = 20;
+    int res = a-b;
+    return res ;
+}
+
